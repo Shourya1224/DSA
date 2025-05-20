cde0af09ff8e339da38d93a5e567ad9e3530349e
Shourya1224 <shourya21m@gmail.com>
2025-05-20 11:35:03 +0530

    hello

---
+++ Demo5.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        int[] arr = { -1, 2, -3, 4, -5, 6, -7, 8 };
+
+        rearrange(arr);
+
+        System.out.println("Rearranged array: " + Arrays.toString(arr));
+    }
+
+    public static void rearrange(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                // Swap negative number to the front
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+}
