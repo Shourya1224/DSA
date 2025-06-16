5422f8fcc359219e35f0111cddef4c0b7360aac2
Shourya1224 <shourya21m@gmail.com>
2025-06-16 15:31:32 +0530

    hello

--- MaximumProductSubarray.java
+++ MaximumProductSubarray.java
@@ -2,8 +2,7 @@ public class MaximumProductSubarray {
     public static int maxProduct(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
-        }
-
+        }   
         int maxProduct = nums[0];
         int maxEndingHere = nums[0];
         int minEndingHere = nums[0];
@@ -14,7 +13,7 @@ public class MaximumProductSubarray {
                 maxEndingHere = minEndingHere;
                 minEndingHere = temp;
             }
-
+                 //shreyayyyyyy
             maxEndingHere = Math.max(nums[i], maxEndingHere * nums[i]);
             minEndingHere = Math.min(nums[i], minEndingHere * nums[i]);
 
@@ -22,8 +21,7 @@ public class MaximumProductSubarray {
         }
 
         return maxProduct;
-    }
-
+    } 
     public static void main(String[] args) {
         int[] nums = {2, 3, -2, 4};
         System.out.println("Maximum product subarray: " + maxProduct(nums));
